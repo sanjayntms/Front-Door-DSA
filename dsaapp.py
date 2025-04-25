f3b7a56c1f61f091cd912c459dff936601aa97ef
sanjayntms <sanjayntms@yahoo.com>
2025-04-25 16:26:55 +0530

    Update dsaapp.py

--- dsaapp.py
+++ dsaapp.py
@@ -1,19 +1,32 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, request, render_template
 import socket
 import datetime
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = message = latency = None
+    start_time = time.time()
 
-@app.route('/api/data')
-def api():
-    return jsonify({
-        'server': socket.gethostname(),
-        'timestamp': datetime.datetime.now().isoformat()
-    })
+    if request.method == 'POST':
+        name = request.form['name']
+        message = request.form['message']
+
+    server = socket.gethostname()
+    current_time = datetime.datetime.utcnow().isoformat()
+    end_time = time.time()
+    latency = round((end_time - start_time) * 1000, 2)
+
+    return render_template(
+        'index.html',
+        name=name,
+        message=message,
+        server=server,
+        time=current_time,
+        latency=latency
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
